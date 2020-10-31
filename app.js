46ce6075fe23f013dce2db607ad56b8cbe593f32
unknown <ramneek983@gmail.com>
2020-10-31 21:14:05 +0530

    First Commit

--- app.js
+++ app.js
@@ -6,9 +6,15 @@ const session = require('express-session')
 const passport = require('passport')
 const passportLocalMongoose =require('passport-local-mongoose')
 const app =express()
+const async = require("async");
+const nodemailer = require("nodemailer");
+const crypto = require("crypto");
+const flash= require('express-flash')
+const { rmdirSync } = require('fs')
+const random = require('mongoose-simple-random')
 app.set('view engine' , 'ejs')
 app.use(bodyParser.urlencoded({extended:true}))
-
+app.use(flash());
 app.use(bodyParser.json());
 app.use(bodyParser.raw());
 
@@ -26,6 +32,12 @@ app.use(session({
 
 mongoose.connect('mongodb://localhost:27017/OnlineExamination', {useNewUrlParser: true});
 mongoose.set('useCreateIndex', true);
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+ });
 
 const QuestionSchema = new mongoose.Schema({
 
@@ -37,12 +49,15 @@ const QuestionSchema = new mongoose.Schema({
     answer : String , 
     ID : Number
 })
+
 const userSchema = new mongoose.Schema({
 
 firstName : String , 
 lastName : String , 
 email : String , 
-password : String
+password : String ,
+resetPasswordToken: String,
+    resetPasswordExpires: Date
 
 })
 userSchema.plugin(passportLocalMongoose , {usernameField: 'email'})
@@ -63,6 +78,7 @@ const supervisorSchema = new mongoose.Schema({
     password : String
 })
 const Supervisor = new mongoose.model("Supervisor" , supervisorSchema)
+adminSchema.plugin(passportLocalMongoose , {usernameField: 'email'})
 const Admin= new mongoose.model("Admin"  , adminSchema)
 const admin= new Admin({
 
@@ -79,13 +95,21 @@ const supervisor = new Supervisor({
     password : "tkkp"
 
 })
-
-
+QuestionSchema.plugin(random)
+const scoreSchema= new mongoose.Schema({
+    email : String , 
+    topic : String ,
+    marks : Number , 
+    time : String
+})
+const Score = new mongoose.model("Score" , scoreSchema)
 
 
 const Question = new  mongoose.model("Question" ,QuestionSchema)
 const User =  new mongoose.model("User", userSchema)
+
 passport.use(User.createStrategy());
+
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 const stack1= new Question ({
@@ -134,6 +158,7 @@ const TopicSchema = new mongoose.Schema({
     questions : [QuestionSchema]
 
 })
+
 const Topic = mongoose.model("Topic" , TopicSchema)
 
 const Array= new Topic({
@@ -254,7 +279,8 @@ app.post('/adminSyllabus/:subtopic' , function(req, res)
                 })
                 
                 console.log(addQues)
-                res.redirect('/')
+                var X= req.params.subtopic
+                res.redirect('/adminSyllabus'+'/'+X)
             }
 
         })
@@ -275,8 +301,9 @@ app.get('/topic' , function(req, res)
 })
 app.get('/syllabus/:subtopic' , function(req, res)
 {
-    console.log(req.params.subtopic)
-    Topic.find({title : req.params.subtopic} , function(err, foundItems)
+   
+
+    Topic.findOne({title : req.params.subtopic} , function(err, foundItems)
     {
         if(err)
         {
@@ -284,15 +311,23 @@ app.get('/syllabus/:subtopic' , function(req, res)
         }
         else
         {
-            console.log(foundItems)
-            res.render('questions' , {topicQuestions : foundItems})
+            
+            const array=foundItems.questions
+            // Shuffle array
+        const shuffled = array.sort(() => 0.5 - Math.random());
+
+    // Get sub-array of first n elements after shuffled
+            let selected = shuffled.slice(0, 5);
+            //console.log(selected)
+            res.render('questions' , {topicQuestions : selected , Topic:req.params.subtopic})
         }
     })
+    //res.render('questions', {topicQuestions:items})
 })
 app.post('/syllabus/:subtopic' , function(req, res)
 {
     var score=0
-    console.log(req.body)
+    console.log(req.user.email)
     Topic.findOne({title : req.params.subtopic} , function(err, foundItems)
     {
         //const Items = foundItems.toObject({ getters: true });
@@ -310,7 +345,7 @@ app.post('/syllabus/:subtopic' , function(req, res)
         
           for (var key of Object.keys(req.body)) {
              
-            console.log(key + " -> " + req.body[key])
+            //console.log(key + " -> " + req.body[key])
             if(key=='action')
             {
                 continue
@@ -319,9 +354,17 @@ app.post('/syllabus/:subtopic' , function(req, res)
             {
                 score=score+1
             }
-            console.log('I am prinitng ' , foundItems.questions[key-1].answer)
+           // console.log('I am prinitng ' , foundItems.questions[key-1].answer)
             
         }
+        const newScore = new Score({
+            email : req.user.email , 
+            topic : req.params.subtopic  , 
+            marks : score ,
+            time :  new Date().toLocaleString()
+        })
+        console.log(newScore)
+        newScore.save()
         
             res.render('score' , {Score : score})
          // console.log(score)
@@ -348,6 +391,7 @@ const firstUser= new User({
 })
 app.post('/login' , function(req, res)
 {
+ 
     const user = new User({
         firstName :" " , 
         lastName :" "  , 
@@ -395,7 +439,6 @@ app.get('/adminLogin' , function(req, res)
 })
 app.post('/adminLogin' , function(req, res)
 {
-    console.log(req.body.email)
     Admin.findOne({email : req.body.email} , function(err, items)
     {
         console.log(items)
@@ -412,6 +455,8 @@ app.post('/adminLogin' , function(req, res)
             }
         }
     })
+
+
 })
 app.get('/supervisorLogin' , function(req, res)
 {
@@ -547,15 +592,22 @@ app.get('/studentAdd' , function(req, res)
 })
 app.post('/studentAdd' ,function(req, res)
 {
-    const addUser= new User({
+    
+    User.register({ email :req.body.email , firstName : req.body.firstName , lastName:req.body.lastName } , req.body.password,function(err , user)
+    {
+        if(err)
+        {
+            console.log(err)
+            res.redirect('/')
+        }
+        else
+        {
+            passport.authenticate("local")(req, res , function(){
+                res.redirect('Syllabus')
+            })
+        }
+    } )
 
-        firstName : req.body.firstName , 
-        lastName : req.body.lastName , 
-        email : req.body.email , 
-        password : req.body.password
-    })
-    addUser.save()
-    res.redirect('supervisorPannel')
 })
 app.get('/adminDel' , function(req, res)
 {
@@ -586,6 +638,7 @@ app.get('/adminAdd' , function(req, res)
 })
 app.post('/adminAdd' , function(req, res)
 {
+    
     const addAdmin= new Admin({
         firstName : req.body.firstName , 
         lastName : req.body.lastName , 
@@ -594,7 +647,207 @@ app.post('/adminAdd' , function(req, res)
     })
     addAdmin.save()
     res.render('supervisorPannel')
+
 })
+app.get('/adminUpdate' , function(req, res)
+{
+    Admin.find({} , function(err, items)
+    {
+        if(err)
+        {
+            console.log(err)
+        }
+        else
+        {
+            res.render('adminUpdate' , {Admins:items})
+        }
+
+    })
+})
+app.get('/adminUpdate/:name' , function(req , res){
+
+
+    Admin.findOne({firstName:req.params.name}, function(err, items)
+    {
+        if(err)
+        {
+            console.log(err)
+        }
+        else{
+            if(items)
+            {
+                res.render('adminUpdateForm' , {Name:req.params.name   , Admin:items})
+            }
+            
+        }
+    })
+})
+app.post('/adminUpdate/:name' , function(req, res)
+{
+    console.log("I am in POST route")
+    console.log(req.params.name)
+    Admin.updateOne({firstName:req.params.name} , {firstName:req.body.firstName , lastName:req.body.lastName , email:req.body.email ,password:req.body.password} ,function(err){
+        if(err)
+        {
+            console.log(err)
+        }
+       
+    })
+    res.redirect('/supervisorPannel')
+})
+
+app.get('/myScore' , function(req, res)
+{
+    Score.find({email : req.user.email}, function(err , items){
+
+        if(err)
+        {
+            console.log(err)
+        }
+        else
+        {
+            console.log("i am in get " , items)
+            res.render('myScores', {Scores: items})
+        }
+    } )
+})
+
+
+
+
+//Adding here
+
+
+
+app.get('/forgot', function(req, res) {
+    res.render('forgot');
+  });
+  
+  app.post('/forgot', function(req, res, next) {
+    async.waterfall([
+      function(done) {
+        crypto.randomBytes(20, function(err, buf) {
+          var token = buf.toString('hex');
+          done(err, token);
+        });
+      },
+      function(token, done) {
+        User.findOne({ email: req.body.email }, function(err, user) {
+          if (!user) {
+            req.flash('error', 'No account with that email address exists.');
+            return res.redirect('/forgot');
+          }
+  
+          user.resetPasswordToken = token;
+          user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+  
+          user.save(function(err) {
+            done(err, token, user);
+          });
+        });
+      },
+      function(token, user, done) {
+        var smtpTransport = nodemailer.createTransport({
+          service: 'Gmail', 
+          auth: {
+            user: 'ramneek983@gmail.com',
+            pass: process.env.GMAILPW
+          }
+        });
+        var mailOptions = {
+          to: user.email,
+          from: 'ramneek@gmail.com',
+          subject: 'Node.js Password Reset',
+          text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
+            'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
+            'http://' + req.headers.host + '/reset/' + token + '\n\n' +
+            'If you did not request this, please ignore this email and your password will remain unchanged.\n'
+        };
+        smtpTransport.sendMail(mailOptions, function(err) {
+          console.log('mail sent');
+          req.flash('success', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
+          done(err, 'done');
+        });
+      }
+    ], function(err) {
+      if (err) return next(err);
+      res.redirect('/login');
+    });
+  });
+  
+  app.get('/reset/:token', function(req, res) {
+    User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
+      if (!user) {
+        req.flash('error', 'Password reset token is invalid or has expired.');
+        return res.redirect('/forgot');
+      }
+      res.render('reset', {token: req.params.token});
+    });
+  });
+  
+  app.post('/reset/:token', function(req, res) {
+    async.waterfall([
+      function(done) {
+        User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
+          if (!user) {
+            req.flash('error', 'Password reset token is invalid or has expired.');
+            return res.redirect('back');
+          }
+          if(req.body.password === req.body.confirm) {
+            user.setPassword(req.body.password, function(err) {
+              user.resetPasswordToken = undefined;
+              user.resetPasswordExpires = undefined;
+  
+              user.save(function(err) {
+                req.logIn(user, function(err) {
+                  done(err, user);
+                });
+              });
+            })
+          } else {
+              req.flash("error", "Passwords do not match.");
+              return res.redirect('back');
+          }
+        });
+      },
+      function(user, done) {
+        var smtpTransport = nodemailer.createTransport({
+          service: 'Gmail', 
+          auth: {
+            user: 'ramneek983@gmail.com',
+            pass: process.env.GMAILPW
+          }
+        });
+        var mailOptions = {
+          to: user.email,
+          from: 'ramneek983@mail.com',
+          subject: 'Your password has been changed',
+          text: 'Hello,\n\n' +
+            'This is a confirmation that the password for your account ' + user.email + ' has just been changed.\n'
+        };
+        smtpTransport.sendMail(mailOptions, function(err) {
+          req.flash('success', 'Success! Your password has been changed.');
+          done(err);
+        });
+      }
+    ], function(err) {
+      res.redirect('/login');
+    });
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.listen(3000 , function()
 {
     console.log("Server running on port 3000")
